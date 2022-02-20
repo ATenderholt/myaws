954100fbae8d556fa553513da630ff94edf1b6db
Adam Tenderholt <atenderholt@gmail.com>
2022-02-20 14:49:58 -0800

    Handler: extend to handle services in auth header

--- http/handler.go
+++ http/handler.go
@@ -7,22 +7,39 @@ import (
 	"regexp"
 )
 
+var authRegex *regexp.Regexp
+
+func init() {
+	temp, err := regexp.Compile(`Credential=(\w+)/\d{8}/([a-z0-9-]+)/(\w+)/aws4_request`)
+	if err != nil {
+		panic(err)
+	}
+
+	authRegex = temp
+}
+
 type route struct {
 	pattern *regexp.Regexp
+	service *string
 	method  string
 	handler http.Handler
 }
 
 type RegexHandler struct {
-	routes []*route
+	regexRoutes   []*route
+	serviceRoutes []*route
 }
 
-func (h *RegexHandler) HandleFunc(pattern string, method string, handler func(http.ResponseWriter, *http.Request)) {
+func (h *RegexHandler) HandleRegex(pattern string, method string, handler func(http.ResponseWriter, *http.Request)) {
 	regex, err := regexp.Compile(pattern)
 	if err != nil {
 		panic(err)
 	}
-	h.routes = append(h.routes, &route{regex, method, http.HandlerFunc(handler)})
+	h.regexRoutes = append(h.regexRoutes, &route{regex, nil, method, http.HandlerFunc(handler)})
+}
+
+func (h *RegexHandler) HandleAuthHeader(service string, method string, handler func(http.ResponseWriter, *http.Request)) {
+	h.serviceRoutes = append(h.serviceRoutes, &route{nil, &service, method, http.HandlerFunc(handler)})
 }
 
 func (h *RegexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -37,14 +54,33 @@ func (h *RegexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		log.Info("   %s : %s", key, value)
 	}
 
-	for _, route := range h.routes {
+	// Handle regex based Routes first
+	for _, route := range h.regexRoutes {
 		if route.pattern.MatchString(r.URL.Path) && route.method == r.Method {
 			route.handler.ServeHTTP(w, r)
 			return
 		}
 	}
 
-	log.Info(" ---- %s %s NOT HANDLED BY REGEX --- ", r.Method, r.URL.Path)
+	auth := r.Header.Get("Authorization")
+	groups := authRegex.FindStringSubmatch(auth)
+	var service string
+	if groups == nil {
+		log.Error("Unable to match Authorization header: %s", auth)
+		service = ""
+	} else {
+		service = groups[3]
+	}
+
+	log.Info("")
+	for _, route := range h.serviceRoutes {
+		if *route.service == service && route.method == r.Method {
+			route.handler.ServeHTTP(w, r)
+		}
+	}
+
+	log.Info(" ---- %s %s NOT HANDLED BY REGEX OR SERVICE [%s] --- ", r.Method, r.URL.Path, service)
+
 	body, _ := io.ReadAll(r.Body)
 	log.Info("Body: %s", body)
 
--- http/server.go
+++ http/server.go
@@ -12,16 +12,18 @@ func Serve() (srv *http.Server, err error) {
 	mux := http.NewServeMux()
 
 	handler := RegexHandler{}
-	handler.HandleFunc(lambda.GetAllLayerVersionsRegex, http.MethodGet, lambda.GetAllLayerVersions)
-	handler.HandleFunc(lambda.GetLayerVersionsRegex, http.MethodGet, lambda.GetLayerVersion)
-	handler.HandleFunc(lambda.PostLayerVersionsRegex, http.MethodPost, lambda.PostLayerVersions)
-	handler.HandleFunc(lambda.GetLambdaFunctionRegex, http.MethodGet, lambda.GetLambdaFunction)
-	handler.HandleFunc(lambda.GetFunctionCodeSigningRegex, http.MethodGet, lambda.GetFunctionCodeSigning)
-	handler.HandleFunc(lambda.GetFunctionVersionsRegex, http.MethodGet, lambda.GetFunctionVersions)
-	handler.HandleFunc(lambda.PostLambdaFunctionRegex, http.MethodPost, lambda.PostLambdaFunction)
-	handler.HandleFunc("/", http.MethodHead, s3.ProxyToMinio)
-	handler.HandleFunc("/", http.MethodGet, s3.ProxyToMinio)
-	handler.HandleFunc("/", http.MethodPut, s3.ProxyToMinio)
+
+	handler.HandleRegex(lambda.GetAllLayerVersionsRegex, http.MethodGet, lambda.GetAllLayerVersions)
+	handler.HandleRegex(lambda.GetLayerVersionsRegex, http.MethodGet, lambda.GetLayerVersion)
+	handler.HandleRegex(lambda.PostLayerVersionsRegex, http.MethodPost, lambda.PostLayerVersions)
+	handler.HandleRegex(lambda.GetLambdaFunctionRegex, http.MethodGet, lambda.GetLambdaFunction)
+	handler.HandleRegex(lambda.GetFunctionCodeSigningRegex, http.MethodGet, lambda.GetFunctionCodeSigning)
+	handler.HandleRegex(lambda.GetFunctionVersionsRegex, http.MethodGet, lambda.GetFunctionVersions)
+	handler.HandleRegex(lambda.PostLambdaFunctionRegex, http.MethodPost, lambda.PostLambdaFunction)
+
+	handler.HandleAuthHeader("s3", http.MethodHead, s3.ProxyToMinio)
+	handler.HandleAuthHeader("s3", http.MethodGet, s3.ProxyToMinio)
+	handler.HandleAuthHeader("s3", http.MethodPut, s3.ProxyToMinio)
 
 	mux.Handle("/", &handler)
 	port := 8080
